2ca64bd7cea2517138cdff83faf964a6793b95cc
Kellyrsch <kelly_schoonderwoerd@hotmail.com>
2026-03-20 14:29:11 +0100

    added feature - point density in convex hull (not sure if this is correct, maybe clearer when features are normalised)

--- code/features/convex_hull.py
+++ code/features/convex_hull.py
@@ -4,3 +4,9 @@ def compute_convex_hull_volume(points):
     hull.orient_triangles()
     hull_volume = hull.get_volume()
     return hull_volume
+
+def point_density_in_convex_hull(points):
+    hull_volume = compute_convex_hull_volume(points)
+    nr_of_pts = len(points.points)
+    point_density = nr_of_pts / hull_volume
+    return point_density
